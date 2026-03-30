83c0e468cbc1dc0f19fc32f422f3be21b9b257ba
xs1ngularx <mengoti24@gmail.com>
2026-03-30 19:11:20 -0300

    Fix asset paths in PreloadScene

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -5,20 +5,18 @@ export default class PreloadScene extends Phaser.Scene {
         this.load.image('dirt-bg', 'assets/dirt-bg.png');
         this.load.spritesheet('miner', 'assets/miner.png', { frameWidth: 512, frameHeight: 512 });
         this.load.image('ui-gear-icon', 'assets/ui-gear-icon.png');
-        // Game Assets
-        this.load.image('game-ore', '../assets/game-ore.png');
-        this.load.image('game-obstacle', '../assets/game-obstacle.png');
-        // Menu Assets
-        this.load.image('menu-bg', '../assets/menu-bg.png');
-        this.load.image('ui-game-title', '../assets/ui-game-title.png');
-        this.load.image('ui-play-button', '../assets/ui-play-button.png');
-        this.load.image('ui-levels-button', '../assets/ui-levels-button.png');
-        this.load.image('ui-shop-button', '../assets/ui-shop-button.png');
-        this.load.image('ui-back-button', '../assets/ui-back-button.png');
-        this.load.image('ui-upgrade-card', '../assets/ui-upgrade-card.png');
+        this.load.image('game-ore', '/assets/game-ore.png');
+        this.load.image('game-obstacle', '/assets/game-obstacle.png');
+        this.load.image('menu-bg', '/assets/menu-bg.png');
+        this.load.image('ui-game-title', '/assets/ui-game-title.png');
+        this.load.image('ui-play-button', '/assets/ui-play-button.png');
+        this.load.image('ui-levels-button', '/assets/ui-levels-button.png');
+        this.load.image('ui-shop-button', '/assets/ui-shop-button.png');
+        this.load.image('ui-back-button', '/assets/ui-back-button.png');
+        this.load.image('ui-upgrade-card', '/assets/ui-upgrade-card.png');
 
         // Audio Assets
-        this.load.audio('deny-purchase', '../assets/deny-purchase.mp3');
+        this.load.audio('deny-purchase', '/assets/deny-purchase.mp3');
     }
     
     create() {
